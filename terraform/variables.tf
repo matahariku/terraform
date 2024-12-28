3e509025509ef8f58ab08b300041ebf54cd81a04
farida eryani <54765115+matahariku@users.noreply.github.com>
2024-12-28 19:51:37 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,36 @@
-variable "grafana_ami" {
-  description = "AMI ID untuk instance Grafana"
-  type        = string
-}
-
-variable "mongodb_ami" {
-  description = "AMI ID untuk instance MongoDB"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Tipe instance AWS"
-  type        = string
-  default     = "t2.micro"
+variable "region" {
+  description = "Region AWS untuk deployment"
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block untuk VPC"
-  type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR block untuk subnet publik"
-  type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block untuk subnet privat"
-  type        = string
   default     = "10.0.2.0/24"
 }
 
-variable "region" {
-  description = "AWS Region untuk deploy"
-  type        = string
-  default     = "us-east-1"
+variable "grafana_ami" {
+  description = "AMI ID untuk instance Grafana"
+}
+
+variable "mongodb_ami" {
+  description = "AMI ID untuk instance MongoDB"
+}
+
+variable "instance_type" {
+  description = "Tipe instance AWS"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "Key pair untuk akses SSH"
-  type        = string
 }
