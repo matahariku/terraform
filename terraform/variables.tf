a93ebdff96172df2109a9027ddc36576b804a3ce
farida eryani <54765115+matahariku@users.noreply.github.com>
2024-12-28 20:25:53 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,151 @@
-variable "region" {
-  description = "Region AWS untuk deployment"
-  type        = string
-  default     = "us-east-1"
+provider "aws" {
+  region = var.region
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block untuk VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+# 1. VPC
+resource "aws_vpc" "nangka" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "nangka"
+  }
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block untuk subnet publik"
-  type        = string
-  default     = "10.0.1.0/24"
+# 2. Internet Gateway
+resource "aws_internet_gateway" "gw_nangka" {
+  vpc_id = aws_vpc.nangka.id
+  tags = {
+    Name = "GW-nangka"
+  }
 }
 
-variable "private_subnet_cidr" {
-  description = "CIDR block untuk subnet privat"
-  type        = string
-  default     = "10.0.2.0/24"
+# 3. Public Subnet
+resource "aws_subnet" "cafe_public" {
+  vpc_id                  = aws_vpc.nangka.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "cafe-public"
+  }
 }
 
-variable "grafana_ami" {
-  description = "AMI ID untuk instance Grafana"
-  type        = string
+# 4. Private Subnet
+resource "aws_subnet" "cafe_private" {
+  vpc_id            = aws_vpc.nangka.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "cafe-privat"
+  }
 }
 
-variable "mongodb_ami" {
-  description = "AMI ID untuk instance MongoDB"
-  type        = string
+# 5. Route Table
+resource "aws_route_table" "route_private" {
+  vpc_id = aws_vpc.nangka.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_nangka.id
+  }
+
+  tags = {
+    Name = "route-privat"
+  }
 }
 
-variable "instance_type" {
-  description = "Tipe instance AWS"
-  type        = string
-  default     = "t2.micro"
+# 6. Associate Route Table with Public Subnet
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.cafe_public.id
+  route_table_id = aws_route_table.route_private.id
 }
 
-variable "key_name" {
-  description = "Key pair untuk akses SSH"
-  type        = string
+# 7. Security Groups
+# Security Group for Grafana
+resource "aws_security_group" "sg_grafana" {
+  vpc_id = aws_vpc.nangka.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG-grafana"
+  }
+}
+
+# Security Group for MongoDB
+resource "aws_security_group" "sg_mongodb" {
+  vpc_id = aws_vpc.nangka.id
+
+  # Allow MongoDB access within VPC
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # Allow SSH access to MongoDB
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Akses global (ganti dengan IP Anda jika perlu)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG-mongodb"
+  }
+}
+
+# 8. Instances
+resource "aws_instance" "grafana" {
+  ami           = var.grafana_ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.cafe_public.id
+  security_groups = [aws_security_group.sg_grafana.id]
+
+  tags = {
+    Name = "Grafana"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = var.mongodb_ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.cafe_public.id
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.sg_mongodb.id]
+
+  tags = {
+    Name = "MongoDB"
+  }
 }
