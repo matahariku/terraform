62079699e83ba988c4260d3506c918d94f1d0751
farida eryani <54765115+matahariku@users.noreply.github.com>
2024-12-28 13:27:38 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,29 +21,3 @@ resource "aws_instance" "mongodb" {
     Name = "MongoDB"
   }
 }
-
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-
-  tags = {
-    Name = "Main VPC"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr
-
-  tags = {
-    Name = "Public Subnet"
-  }
-}
-
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr
-
-  tags = {
-    Name = "Private Subnet"
-  }
-}
