b2d4048ab4231f5fe11b34074532b57999cf7f19
farida eryani <54765115+matahariku@users.noreply.github.com>
2025-01-02 12:39:24 +0100

    Update provider.tf

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -5,18 +5,18 @@ terraform {
       version = "5.60.0"
     }
   }
-}
 
- backend "s3" {
-    bucket         = "deploy-aws-terra"         
-    key            = "terraform/state/aws/terraform.tfstate" 
-    region         = "us-east-1"               
-    dynamodb_table = "terraform-lock-table"    
-    encrypt        = true                     
+  backend "s3" {
+    bucket         = "deploy-aws-terra"         # Nama bucket S3 Anda
+    key            = "terraform/state/aws/terraform.tfstate" # Lokasi file state di bucket
+    region         = "us-east-1"               # Wilayah AWS
+    dynamodb_table = "terraform-lock-table"    # Tabel DynamoDB untuk lock
+    encrypt        = true                      # Enkripsi file state
   }
 }
 
 provider "aws" {
   default_tags {
+    # Tambahkan tags default jika perlu
   }
 }
