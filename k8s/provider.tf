8f839acf4f55499de7609e009ca5f4e4893130a3
farida eryani <54765115+matahariku@users.noreply.github.com>
2025-01-02 12:38:27 +0100

    Update provider.tf

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+ backend "s3" {
+    bucket         = "deploy-aws-terra"         
+    key            = "terraform/state/aws/terraform.tfstate" 
+    region         = "us-east-1"               
+    dynamodb_table = "terraform-lock-table"    
+    encrypt        = true                     
+  }
+}
+
 provider "aws" {
   default_tags {
   }
