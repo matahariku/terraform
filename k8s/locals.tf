fbfb32d5814631a1138532681a2279a09d2d28eb
farida eryani <54765115+matahariku@users.noreply.github.com>
2025-01-02 12:17:09 +0100

    Create locals.tf

---
+++ k8s/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  ami                  = "ami-04a81a99f5ec58529"
+  instance_type        = "t3.medium"
+  key_name             = "vockey"
+  iam_instance_profile = "LabInstanceProfile"
+}
+
+locals {
+  tags = {
+    "kubernetes.io/cluster/kubernetes" = "owned"
+  }
+}
