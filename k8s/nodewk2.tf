62ec85a03c8e967c5530f97a3fdf377f19b16b78
farida eryani <54765115+matahariku@users.noreply.github.com>
2025-01-02 12:18:52 +0100

    Create nodewk2.tf

---
+++ k8s/nodewk2.tf
@@ -0,0 +1,35 @@
+module "wknode2" {
+  depends_on = [module.vpc]
+  source     = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "wknode2"
+
+  ami                         = local.ami
+  instance_type               = local.instance_type
+  key_name                    = local.key_name
+  monitoring                  = false
+  user_data                   = data.cloudinit_config.worker.rendered
+  associate_public_ip_address = false
+  vpc_security_group_ids = [
+    module.worker_sg.security_group_id,
+    module.inter-nodes_sg.security_group_id
+  ]
+  subnet_id            = module.vpc.private_subnets[0]
+  iam_instance_profile = local.iam_instance_profile
+  tags                 = local.tags
+}
+
+resource "aws_ebs_volume" "osd2" {
+  availability_zone = "us-east-1a"
+  size              = 30
+}
+
+resource "aws_volume_attachment" "osd2_att" {
+  device_name = "/dev/xvdb"
+  volume_id   = aws_ebs_volume.osd2.id
+  instance_id = module.wknode2.id
+}
+
+output "wknode2_private_dns" {
+  value = module.wknode2.private_dns
+}
