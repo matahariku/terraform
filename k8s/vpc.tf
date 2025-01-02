650ef6789ffe019d9ec36972401084cc58f9dd8e
farida eryani <54765115+matahariku@users.noreply.github.com>
2025-01-02 12:29:58 +0100

    Update vpc.tf

--- k8s/vpc.tf
+++ k8s/vpc.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "kube-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.2.0/24"]
+  private_subnets = ["10.1.1.0/24"]
+  public_subnets  = ["10.1.2.0/24"]
 
   enable_nat_gateway = true
   tags               = local.tags
