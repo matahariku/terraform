30b04ae6f028eaa761a88d059340a5a33b5146ff
farida eryani <54765115+matahariku@users.noreply.github.com>
2025-01-03 18:47:42 +0100

    Update provider.tf

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -6,15 +6,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "terra-farida"         # Name bucket S3
-    key            = "terraform/state/aws/terraform.tfstate" # Location file state
-    region         = "us-east-1"               
-    dynamodb_table = "terraform-lock-table"    # DynamoDB for state locking
-    encrypt        = true                      
-  }
-}
-
 provider "aws" {
   region = "us-east-1" 
 
